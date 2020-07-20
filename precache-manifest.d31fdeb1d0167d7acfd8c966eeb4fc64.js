03be7dfeea03328fed1352641905f9178f8cc808
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2020-07-20 22:55:33 +0530

    Updates

---
+++ precache-manifest.d31fdeb1d0167d7acfd8c966eeb4fc64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "064409aa37bd401388348f93beb066c3",
+    "url": "/flowapp/index.html"
+  },
+  {
+    "revision": "3845ac83efceaddf66ce",
+    "url": "/flowapp/static/css/main.901240e5.chunk.css"
+  },
+  {
+    "revision": "64e937181392bad83f9d",
+    "url": "/flowapp/static/js/2.bb33719f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/flowapp/static/js/2.bb33719f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3845ac83efceaddf66ce",
+    "url": "/flowapp/static/js/main.8a875c90.chunk.js"
+  },
+  {
+    "revision": "ab0cb3b1c317fb8e0ed4",
+    "url": "/flowapp/static/js/runtime-main.875ecf16.js"
+  }
+]);
